7dac820f615e667ff0a435ae5cd4aaefc77421af
nicolasspring <springnicolas@hispeed.ch>
2020-01-29 18:46:20 +0100

    class for extracting URLs

---
+++ URLFinder.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Nicolas Spring
+
+import argparse
+import os
+import pandas as pd
+import re
+import sys
+
+from typing import Dict
+
+
+class URLFinder(object):
+
+    def __init__(self, verbose: int = 1):
+        '''
+        Args:
+        verbose     the verbosity level.
+        '''
+        self.verbose = verbose
+        self.all_files = None
+        self.column_idx = None
+        self.df = None
+        self.df_url = None
+        self.document_dir = None
+        self.input_file = None
+        self.url_dict = None
+
+    def create_url_dict(self, document_dir: str):
+        '''
+        Creates a dictionary containing the URL for every title of an article in @param document_dir.
+
+        Args:        
+        input_dir   a directory containing files in the medialab document format.
+                        http://medialab.di.unipi.it/wiki/Document_Format
+        '''
+        self.document_dir = document_dir
+        self.all_files = [os.path.join(root, file) for root, _, files in os.walk(self.document_dir) for file in files]
+        self._debug(f'Number of files found:\t{len(self.all_files)}')
+        self._debug('Extracting URLs from files...')
+        result_list = []
+        for file in self.all_files:
+            result = self._find_title_url(file)
+            result_list += [result]
+        self.url_dict = {key:value for d in result_list for key, value in d.items()}
+        self._debug(f'Sucessfully extracted the URLs for {len(self.url_dict)} articles.')
+
+    def add_url_column(self, input_file: str, column_idx: int, output_file: str):
+        '''
+        Adds a column containing the URLs for every title in a specified (by index) column.
+
+        Args:
+        input_file      the name of a tsv file containig a column of article title to be mapped to URLs.
+        column_idx      the index of the column containing the article titles.
+        output_file     the output file where the new file will be saved.
+        '''
+        self._debug(f'Adding a column with URLs and saving to {output_file}...')
+        self.input_file = input_file
+        self.column_idx = column_idx
+        self.output_file = output_file
+        self.df = pd.read_csv(input_file, sep='\t', quotechar="'", header=None)
+        url_list = []
+        warned = []
+        for _, row in self.df.iterrows():
+            try:
+                url_list.append(self.url_dict[row[column_idx]])
+            except KeyError:
+                if row[column_idx] not in warned:
+                    self._debug(f'Could not find the URL for article title "{row[column_idx]}".', prefix='WARNING:\t')
+                    url_list.append(None)
+                    warned.append(row[column_idx])
+        self.df_url = self.df.copy()
+        self.df_url.insert(column_idx+1, column_idx+1, pd.Series(url_list))
+        self.df_url.to_csv(output_file, sep='\t', quotechar="'", index=False, header=False)
+        self._debug(f'Failed to find URLs for {len(warned)} articles.', prefix='WARNING\t')
+        self._debug(f'Task completed and output saved to {output_file}.')
+
+    def _find_title_url(self, doc_file: str) -> Dict[str, str]:
+        '''
+        Searches a file in medialab document format and returns a dictionary with article
+        names as keys and URLs as values.
+        '''
+        with open(doc_file, encoding='utf8') as infile:
+            part_dict = {}
+            for line in infile:
+                if line.startswith('<doc id='):
+                    head = line.lstrip('<doc ').rstrip('>')
+                    title = re.search(r'title=(.*")', head).group(1).strip('"')
+                    url = re.search(r'url=(\S*) ', head).group(1).strip('"')
+                    part_dict[title] = url
+        return part_dict
+
+    def _debug(self, message: str, level: int = 1, prefix: str = 'INFO:\t', spacing: str = '', end_spacing: str = ''):
+        '''
+        prints debug messages according to the verbosity level.
+        '''
+        if self.verbose >= level:
+            output = sys.stderr if not self.verbose > 50 else sys.stdout
+            print(spacing + prefix + message + end_spacing, file=output)
---
+++ parse_documents.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Nicolas Spring
+
+
+import argparse
+import pathos.multiprocessing as mp
+import spacy
+
+from DocumentParser import DocumentParser
+from URLFinder import URLFinder
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--files', type=int,
+                        help='The number of files handled per process before writing to the output file.',
+                        default=1, metavar='INT')
+    parser.add_argument('-i', '--input', type=str,
+                        help='A directory containing files in medialab document format for parsing.',
+                        metavar='PATH', required=True)
+    parser.add_argument('-m', '--match', type=str,
+                        help='Output file for articles with a match. ' + \
+                        'If no --match-lang is provided, all output will be written to this file.',
+                        metavar='PATH')
+    parser.add_argument('-n', '--no-match', type=str,
+                        help='Output file for articles with no match.',
+                        metavar='PATH', default=None)
+    parser.add_argument('-p', '--processes', type=int,
+                        help='The number of processes to be run in parallel.',
+                        default=mp.cpu_count(), metavar='INT')
+    parser.add_argument('-v' ,'--verbose', type=int, metavar='INT',
+                        help='The verbosity level of the DocumentParser.', default=1)
+    parser.add_argument('--db-user', type=str, metavar='STRING',
+                        help='The mysql databank user with access to the langlinks table.')
+    parser.add_argument('--db-host', type=str, metavar='STRING',
+                        help='The host of the databank containing the langlinks table.')
+    parser.add_argument('--db-database', type=str, metavar='STRING',
+                        help='The mysql database containing the langlinks table.')
+    parser.add_argument('--input-lang', type=str, metavar='STRING',
+                        help='The language of the input files (EN/DE)',
+                        choices=['DE', 'EN'], default='EN')
+    parser.add_argument('--input-urls', type=str, metavar='STRING',
+                        help='A directory containing files in medialab document format for extracting other language URLs.')
+    parser.add_argument('--match-lang', type=str, metavar='STRING', default='de',
+                        help='The Wikipedia language code for title matches (default: de).')
+    parser.add_argument('--output-url-file', type=str, metavar='STRING',
+                        help='The output file for the tsv with added URL for corresponding articles in other language.')
+    args = parser.parse_args()
+    return args
+
+
+def main(args: argparse.Namespace):
+    # parsing the documents and writing to a tsv file
+    spacy_model = spacy.load('en_core_web_sm') if args.input_lang.upper() == 'EN' else spacy.load('de_core_news_sm')
+    databank_login = {'user':args.db_user, 'host':args.db_host, 'database':args.db_database}
+    doc_parser = DocumentParser(spacy_model,
+                                args.input,
+                                args.match,
+                                match_lang=args.match_lang,
+                                no_match_file=args.no_match,
+                                mysql_dict=databank_login,
+                                n_processes=args.processes,
+                                n_files=args.files,
+                                verbose=args.verbose)
+    column_id_dict = doc_parser.parse_documents()
+
+    # adding a column with URLs of articles in the other language
+    finder = URLFinder(verbose=args.verbose)
+    finder.create_url_dict(args.input_urls)
+    finder.add_url_column(args.match, column_id_dict['other_title'], args.output_url_file)
+        
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
