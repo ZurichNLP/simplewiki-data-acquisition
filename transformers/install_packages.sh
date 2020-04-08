3e93d8ba4b9f01c24a8e12a4e69427790636f10f
nicolasspring <springnicolas@hispeed.ch>
2020-04-08 23:21:30 +0200

    sacrebleu 1.4.4

--- transformers/install_packages.sh
+++ transformers/install_packages.sh
@@ -7,6 +7,9 @@ cd $TRANSFORMERS
 
 conda install pytorch torchvision cudatoolkit=10.0 -c pytorch
 
-pip install Cython
+pip install Cython sacremoses subword_nmt sacrebleu==1.4.4
 
-pip install fairseq
+mkdir -p $TRANSFORMERS/software
+git clone https://github.com/pytorch/fairseq $TRANSFORMERS/software/fairseq
+cd $TRANSFORMERS/software/fairseq
+pip install .
